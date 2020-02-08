24954cc4526069ec1e2c2d37f4a55b1839035153
Matti Luukkainen <mluukkai@iki.fi>
2020-02-08 18:52:28 +0200

    part6-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import NewNote from './components/NowNote'
 import Notes from './components/Notes'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
-
   return (
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
